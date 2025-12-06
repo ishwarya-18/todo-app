24147dd4634a149a20f230803df8d20525db1d60
ISHWARYA R <ishwarya.ad23@bitsathy.ac.in>
2025-12-06 10:02:43 +0530

    Removed env

--- server/server.js
+++ server/server.js
@@ -11,8 +11,11 @@ const PORT = process.env.PORT || 5000;
 // Database connection
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 // Middleware
 app.use(cors());
